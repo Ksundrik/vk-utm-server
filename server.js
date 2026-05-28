72f1b1c4257519ebc942c64086dd190a6fbf7a48
Ksundrik <i.kseniakos@gmail.com>
2026-05-28 21:04:48 +0300

    Update server.js

--- server.js
+++ server.js
@@ -10,6 +10,27 @@ app.get('/', (req, res) => {
   res.send('Server works');
 });
 
+const storage = {};
+
+app.post('/save', (req, res) => {
+  const { utm_source } = req.body;
+
+  if (!utm_source) {
+    return res.status(400).send('No utm_source');
+  }
+
+  const id = Date.now().toString();
+
+  storage[id] = {
+    utm_source,
+    createdAt: Date.now()
+  };
+
+  console.log('Saved:', storage[id]);
+
+  res.json({ id });
+});
+
 app.listen(3000, () => {
   console.log('Server started');
 });
